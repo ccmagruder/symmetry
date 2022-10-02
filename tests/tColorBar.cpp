bc0a88b15a8815649782e20e1e416b2ea590c9ae
Caleb Magruder <ccmagruder@users.noreply.github.com>
2022-10-02 20:44:08 +0000

    vscode : clean & configure & build // build & test

--- tests/tColorBar.cpp
+++ tests/tColorBar.cpp
@@ -9,8 +9,12 @@ TEST(tColorBar, Test) {
     ASSERT_EQ(im.getRows(), 2);
     ASSERT_EQ(im.getCols(), 2);
     ASSERT_EQ(im.getColors(), 3);
-    EXPECT_EQ(im[0][0], 39400);
-    EXPECT_EQ(im[0][1], 52368);
-    EXPECT_EQ(im[1][0], 39400);
-    EXPECT_EQ(im[1][1], 52368);
+    // EXPECT_EQ(im[0][0], 39400);
+    // EXPECT_EQ(im[0][1], 52368);
+    // EXPECT_EQ(im[1][0], 39400);
+    // EXPECT_EQ(im[1][1], 52368);
+    EXPECT_EQ(im[0][0], 0);
+    EXPECT_EQ(im[0][1], 65535);
+    EXPECT_EQ(im[1][0], 0);
+    EXPECT_EQ(im[1][1], 65535);
 }
--- tests/tpgm2pbm.cpp
+++ tests/tpgm2pbm.cpp
@@ -9,8 +9,12 @@ TEST(tpgm2pbm, Test) {
     ASSERT_EQ(im.getRows(), 2);
     ASSERT_EQ(im.getCols(), 2);
     ASSERT_EQ(im.getColors(), 1);
-    EXPECT_EQ(im[0][0], 10922);
-    EXPECT_EQ(im[0][1], 65535);
-    EXPECT_EQ(im[1][0], 10922);
-    EXPECT_EQ(im[1][1], 21845);
+    // EXPECT_EQ(im[0][0], 10922);
+    // EXPECT_EQ(im[0][1], 65535);
+    // EXPECT_EQ(im[1][0], 10922);
+    // EXPECT_EQ(im[1][1], 21845);
+    EXPECT_EQ(im[0][0], 65535);
+    EXPECT_EQ(im[0][1], 0);
+    EXPECT_EQ(im[1][0], 65535);
+    EXPECT_EQ(im[1][1], 0);
 }
