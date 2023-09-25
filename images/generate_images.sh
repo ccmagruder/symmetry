2a8ec8a08d784c08b17d138c2675a567f41572a5
Caleb Magruder <ccmagruder@gmail.com>
2023-09-25 09:14:31 -0500

    removed : im1-*.pgm -> im1-10.pgm

--- images/generate_images.sh
+++ images/generate_images.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-for j in 5 6 7 8 9 10 11
-do
-    for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
-    do
-        sed "s/1e7/1e$j/" config/fig$i.json > config/fig$i-$j.json
-        build/symmetry run config/fig$i-$j.json images/im$i-$j.pgm 2>/dev/null
-        rm config/fig$i-$j.json
-    done
-done
+#for j in 5 6 7 8 9 10 11
+#do
+#    for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+#    do
+#        sed "s/2e7/2e$j/" config/fig$i.json > config/fig$i-$j.json
+#        build/symmetry run config/fig$i-$j.json images/im$i-$j.pgm 2>/dev/null
+#        rm config/fig$i-$j.json
+#    done
+#done
+
+build/symmetry run config/fig1-10.json images/im1-10.pgm
+
+
+
