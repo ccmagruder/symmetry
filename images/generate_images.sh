8c89e4b8dc19037b7035e33aef1ab86c281f5be5
Caleb Magruder <ccmagruder@gmail.com>
2023-09-25 14:41:20 -0500

    removed : im8-*.pgm -> im8-7.pgm

--- images/generate_images.sh
+++ images/generate_images.sh
@@ -4,4 +4,9 @@ build/symmetry run config/fig1-10.json images/im1-10.pgm
 build/symmetry run config/fig2-9.json images/im2-9.pgm
 build/symmetry run config/fig3-11.json images/im3-11.pgm
 build/symmetry run config/fig4-8.json images/im4-8.pgm
+build/symmetry run config/fig5-7.json images/im5-7.pgm
+build/symmetry run config/fig6-11.json images/im6-11.pgm
+build/symmetry run config/fig7-7.json images/im7-7.pgm
+build/symmetry run config/fig7-11.json images/im7-11.pgm
+build/symmetry run config/fig8-7.json images/im8-7.pgm
 
