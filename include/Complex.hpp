9d29d0d2a01eab25edcd946830f642d1e3ff4b3a
Caleb Magruder <ccmagruder@gmail.com>
2023-11-26 16:30:17 +0000

    added : Complex::operator*=, Complex::operator=

--- include/Complex.hpp
+++ include/Complex.hpp
@@ -2,16 +2,39 @@
 
 #pragma once
 
-#include <complex>
-
 template<typename T>
 class Complex{
  public:
-    explicit Complex(size_t N){
-        _ptr = malloc(N*sizeof(T));
+    explicit Complex(size_t N) : _N(N) {
+        this->_ptr = new T[N];
     }
-    ~Complex(){ free(_ptr); }
+    ~Complex(){ delete[] _ptr; }
+
+    Complex& operator=(const Complex<T>& other) {
+        assert(this->_N == other._N);
+        for (ptrdiff_t i = 0; i < this->_N; i++) {
+            this->_ptr[i] = other[i];
+        }
+        return *this;
+    }
+
+    T& operator[](ptrdiff_t i) {
+        return this->_ptr[i];
+    }
+
+    const T& operator[](ptrdiff_t i) const {
+        return this->_ptr[i];
+    }
+
+    Complex<T>& operator*=(const Complex<T>& other) {
+        assert(this->_N == other._N);
+        for (ptrdiff_t i = 0; i < this->_N; i++) {
+            this->_ptr[i] *= other[i];
+        }
+        return *this;
+    }
  private:
-    void* _ptr;
+    size_t _N;
+    T* _ptr;
 };
 
--- tests/tComplex.cpp
+++ tests/tComplex.cpp
@@ -1,9 +1,30 @@
-// Copyright 2023
+// Copyright 2023 Caleb Magruder
+
+#include <complex>
 
 #include "gtest/gtest.h"
 
 #include "Complex.hpp"
 
 TEST(tComplex, Ctor) {
-    Complex<double> vec(2);
+    Complex<std::complex<double>> vec(2);
+    vec[0] = {3, 4};
+    EXPECT_EQ(vec[0], std::complex<double>(3, 4));
+}
+
+TEST(tComplex, Multiplication) {
+    Complex<std::complex<double>> vec1(1), vec2(1);
+    vec1[0] = {0, 1};
+    vec2[0] = {1, -1};
+    vec2 *= vec1;
+    EXPECT_EQ(vec2[0], std::complex<double>(1, 1));
+}
+
+TEST(tComplex, Assignment) {
+    Complex<std::complex<double>> vec1(2);
+    vec1[0] = {1, -1};
+    vec1[1] = {0, 1};
+    Complex<std::complex<double>> vec2(2);
+    vec2 = vec1;
+    EXPECT_EQ(vec2[1], std::complex<double>(0, 1));
 }
