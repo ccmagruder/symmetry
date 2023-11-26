2be3dbeebf52b55caa80ecfe0b1d389e249c1a4d
Caleb Magruder <ccmagruder@gmail.com>
2023-11-26 18:33:19 +0000

    added : tComplex typed tests

--- tests/tComplex.cpp
+++ tests/tComplex.cpp
@@ -6,74 +6,84 @@
 
 #include "Complex.hpp"
 
-TEST(tComplex, Ctor) {
-    Complex<std::complex<double>> vec(2);
+template <typename T>
+class tComplex : public testing::Test {};
+
+using Types = ::testing::Types <
+    std::complex<float>,
+    std::complex<double>
+>;
+
+TYPED_TEST_SUITE(tComplex, Types);
+
+TYPED_TEST(tComplex, Ctor) {
+    Complex<TypeParam> vec(2);
     vec[0] = {3, 4};
-    EXPECT_EQ(vec[0], std::complex<double>(3, 4));
+    EXPECT_EQ(vec[0], TypeParam(3, 4));
 }
 
-TEST(tComplex, Addition) {
-    Complex<std::complex<double>> x(1), y(1), z(1);
+TYPED_TEST(tComplex, Addition) {
+    Complex<TypeParam> x(1), y(1), z(1);
     x[0] = {0, 1};
     y[0] = {1, -1};
     z = x + y;
-    EXPECT_EQ(z[0], std::complex<double>(1, 0));
+    EXPECT_EQ(z[0], TypeParam(1, 0));
 }
 
-TEST(tComplex, Multiplication) {
-    Complex<std::complex<double>> x(1), y(1), z(1);
+TYPED_TEST(tComplex, Multiplication) {
+    Complex<TypeParam> x(1), y(1), z(1);
     x[0] = {0, 1};
     y[0] = {1, -1};
     z = x * y;
-    EXPECT_EQ(z[0], std::complex<double>(1, 1));
+    EXPECT_EQ(z[0], TypeParam(1, 1));
 }
 
-TEST(tComplex, Assignment) {
-    Complex<std::complex<double>> vec1(2);
+TYPED_TEST(tComplex, Assignment) {
+    Complex<TypeParam> vec1(2);
     vec1[0] = {1, -1};
     vec1[1] = {0, 1};
-    Complex<std::complex<double>> vec2(2);
+    Complex<TypeParam> vec2(2);
     vec2 = vec1;
-    EXPECT_EQ(vec2[1], std::complex<double>(0, 1));
+    EXPECT_EQ(vec2[1], TypeParam(0, 1));
 }
 
-TEST(tComplex, ScalarMultiplication) {
-    Complex<std::complex<double>> vec1(1), vec2(1);
+TYPED_TEST(tComplex, ScalarMultiplication) {
+    Complex<TypeParam> vec1(1), vec2(1);
     vec1[0] = {-1, 1};
-    std::complex<double> a(0, 1);
+    TypeParam a(0, 1);
     vec2 = a * vec1;
-    EXPECT_EQ(vec2[0], std::complex<double>(-1, -1));
+    EXPECT_EQ(vec2[0], TypeParam(-1, -1));
 }
 
-TEST(tComplex, Abs) {
-    Complex<std::complex<double>> vec1(2), vec2(2);
+TYPED_TEST(tComplex, Abs) {
+    Complex<TypeParam> vec1(2), vec2(2);
     vec1[0] = {3, -4};
     vec1[1] = {0, -1};
     vec2 = abs(vec1);
-    EXPECT_EQ(vec2[0], std::complex<double>(5, 0));
-    EXPECT_EQ(vec2[1], std::complex<double>(1, 0));
+    EXPECT_EQ(vec2[0], TypeParam(5, 0));
+    EXPECT_EQ(vec2[1], TypeParam(1, 0));
 }
 
-TEST(tComplex, Arg) {
-    Complex<std::complex<double>> vec1(2), vec2(2);
+TYPED_TEST(tComplex, Arg) {
+    Complex<TypeParam> vec1(2), vec2(2);
     vec1[0] = {-1, 0};
     vec1[1] = {0, -1};
     vec2 = arg(vec1);
-    EXPECT_EQ(vec2[0], std::complex<double>(std::numbers::pi, 0));
-    EXPECT_EQ(vec2[1], std::complex<double>(-std::numbers::pi/2, 0));
+    EXPECT_EQ(vec2[0], TypeParam(std::numbers::pi, 0));
+    EXPECT_EQ(vec2[1], TypeParam(-std::numbers::pi/2, 0));
 }
 
-TEST(tComplex, Conj) {
-    Complex<std::complex<double>> vec1(2), vec2(2);
+TYPED_TEST(tComplex, Conj) {
+    Complex<TypeParam> vec1(2), vec2(2);
     vec1[0] = {-1, 0};
     vec1[1] = {1, -2};
     vec2 = conj(vec1);
-    EXPECT_EQ(vec2[0], std::complex<double>(-1, 0));
-    EXPECT_EQ(vec2[1], std::complex<double>(1, 2));
+    EXPECT_EQ(vec2[0], TypeParam(-1, 0));
+    EXPECT_EQ(vec2[1], TypeParam(1, 2));
 }
 
-TEST(tComplex, Cos) {
-    Complex<std::complex<double>> vec1(2), vec2(2);
+TYPED_TEST(tComplex, Cos) {
+    Complex<TypeParam> vec1(2), vec2(2);
     vec1[0] = {std::numbers::pi, 4};
     vec1[1] = {std::numbers::pi/2, -2};
     vec2 = cos(vec1);
