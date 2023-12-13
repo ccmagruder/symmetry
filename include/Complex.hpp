6d1074175d0f300cf9df9d9885581dd3afd02b9d
Caleb Magruder <ccmagruder@gmail.com>
2023-12-13 02:07:36 +0000

    added : Complex:: using Type=typename T::value_type

--- include/Complex.hpp
+++ include/Complex.hpp
@@ -11,6 +11,7 @@ class gpuDoubleComplex {
 
 template<typename T>
 class Complex{
+    using Type=typename T::value_type;
  public:
     explicit Complex(size_t N) : _N(N) {
         this->_ptr = reinterpret_cast<void*>(new T[N]);
@@ -19,21 +20,18 @@ class Complex{
 
     Complex<T>& operator=(const Complex<T>& other) {
         assert(this->_N == other._N);
-        std::complex<typename T::value_type>* ptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(
-                this->_ptr);
-        std::complex<typename T::value_type>* other_ptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(
-                other._ptr);
+        std::complex<Type>* ptr
+            = reinterpret_cast<std::complex<Type>*>(this->_ptr);
+        std::complex<Type>* other_ptr
+            = reinterpret_cast<std::complex<Type>*>(other._ptr);
         for (ptrdiff_t i = 0; i < this->_N; i++) {
             *ptr++ = *other_ptr++;
         }
         return *this;
     }
 
-    Complex<T>& operator=(std::initializer_list<typename T::value_type> l) {
-        typename T::value_type* ptr
-            = reinterpret_cast<typename T::value_type*>(this->_ptr);
+    Complex<T>& operator=(std::initializer_list<Type> l) {
+        Type* ptr = reinterpret_cast<Type*>(this->_ptr);
         for (auto i = l.begin(); i < l.end(); i++) {
             *ptr = *i;
             ptr++;
@@ -41,9 +39,8 @@ class Complex{
         return *this;
     }
 
-    bool operator==(std::initializer_list<typename T::value_type> l) const {
-        typename T::value_type* ptr
-            = reinterpret_cast<typename T::value_type*>(this->_ptr);
+    bool operator==(std::initializer_list<Type> l) const {
+        Type* ptr = reinterpret_cast<Type*>(this->_ptr);
         for (auto i = l.begin(); i < l.end(); i++) {
             if (*ptr != *i) {
                 return false;
@@ -64,12 +61,12 @@ class Complex{
     friend Complex<T> operator+(const Complex<T>& x, const Complex<T>& y) {
         assert(x._N == y._N);
         Complex<T> z(x._N);
-        std::complex<typename T::value_type>* xptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(x._ptr);
-        std::complex<typename T::value_type>* yptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(y._ptr);
-        std::complex<typename T::value_type>* zptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(z._ptr);
+        std::complex<Type>* xptr
+            = reinterpret_cast<std::complex<Type>*>(x._ptr);
+        std::complex<Type>* yptr
+            = reinterpret_cast<std::complex<Type>*>(y._ptr);
+        std::complex<Type>* zptr
+            = reinterpret_cast<std::complex<Type>*>(z._ptr);
         for (ptrdiff_t i = 0; i < x._N; i++) {
             *zptr++ = *xptr++ + *yptr++;
         }
@@ -79,12 +76,12 @@ class Complex{
     friend Complex<T> operator*(const Complex<T>& x, const Complex<T>& y) {
         assert(x._N == y._N);
         Complex<T> z(x._N);
-        std::complex<typename T::value_type>* xptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(x._ptr);
-        std::complex<typename T::value_type>* yptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(y._ptr);
-        std::complex<typename T::value_type>* zptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(z._ptr);
+        std::complex<Type>* xptr
+            = reinterpret_cast<std::complex<Type>*>(x._ptr);
+        std::complex<Type>* yptr
+            = reinterpret_cast<std::complex<Type>*>(y._ptr);
+        std::complex<Type>* zptr
+            = reinterpret_cast<std::complex<Type>*>(z._ptr);
         for (ptrdiff_t i = 0; i < x._N; i++) {
             *zptr++ = *xptr++ * *yptr++;
         }
@@ -93,12 +90,12 @@ class Complex{
 
     friend Complex<T> operator*(const T& a, const Complex<T>& x) {
         Complex<T> y(x._N);
-        const std::complex<typename T::value_type>* aptr
-            = reinterpret_cast<const std::complex<typename T::value_type>*>(&a);
-        std::complex<typename T::value_type>* xptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(x._ptr);
-        std::complex<typename T::value_type>* yptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(y._ptr);
+        const std::complex<Type>* aptr
+            = reinterpret_cast<const std::complex<Type>*>(&a);
+        std::complex<Type>* xptr
+            = reinterpret_cast<std::complex<Type>*>(x._ptr);
+        std::complex<Type>* yptr
+            = reinterpret_cast<std::complex<Type>*>(y._ptr);
         for (ptrdiff_t i = 0; i < x._N; i++) {
             *yptr++ = *aptr * *xptr++;
         }
@@ -107,10 +104,10 @@ class Complex{
 
     friend Complex<T> abs(const Complex<T>& x) {
         Complex<T> y(x._N);
-        std::complex<typename T::value_type>* xptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(x._ptr);
-        typename T::value_type* yptr
-            = reinterpret_cast<typename T::value_type*>(y._ptr);
+        std::complex<Type>* xptr
+            = reinterpret_cast<std::complex<Type>*>(x._ptr);
+        Type* yptr
+            = reinterpret_cast<Type*>(y._ptr);
         for (ptrdiff_t i = 0; i < x._N; i++) {
             *yptr++ = abs(*xptr++);
             *yptr++ = 0;
@@ -120,10 +117,10 @@ class Complex{
 
     friend Complex<T> arg(const Complex<T>& x) {
         Complex<T> y(x._N);
-        std::complex<typename T::value_type>* xptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(x._ptr);
-        typename T::value_type* yptr
-            = reinterpret_cast<typename T::value_type*>(y._ptr);
+        std::complex<Type>* xptr
+            = reinterpret_cast<std::complex<Type>*>(x._ptr);
+        Type* yptr
+            = reinterpret_cast<Type*>(y._ptr);
         for (ptrdiff_t i = 0; i < x._N; i++) {
             *yptr++ = arg(*xptr++);
             *yptr++ = 0;
@@ -133,10 +130,10 @@ class Complex{
 
     friend Complex<T> conj(const Complex<T>& x) {
         Complex<T> y(x._N);
-        std::complex<typename T::value_type>* xptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(x._ptr);
-        std::complex<typename T::value_type>* yptr
-            = reinterpret_cast<std::complex<typename T::value_type>*>(y._ptr);
+        std::complex<Type>* xptr
+            = reinterpret_cast<std::complex<Type>*>(x._ptr);
+        std::complex<Type>* yptr
+            = reinterpret_cast<std::complex<Type>*>(y._ptr);
         for (ptrdiff_t i = 0; i < x._N; i++) {
             *yptr++ = conj(*xptr++);
         }
@@ -145,10 +142,10 @@ class Complex{
 
     friend Complex<T> cos(const Complex<T>& x) {
         Complex<T> y(x._N);
-        typename T::value_type* xptr
-            = reinterpret_cast<typename T::value_type*>(x._ptr);
-        typename T::value_type* yptr
-            = reinterpret_cast<typename T::value_type*>(y._ptr);
+        Type* xptr
+            = reinterpret_cast<Type*>(x._ptr);
+        Type* yptr
+            = reinterpret_cast<Type*>(y._ptr);
         for (ptrdiff_t i = 0; i < x._N; i++) {
             *yptr++ = cos(*xptr++);
             *yptr++ = *xptr++;
