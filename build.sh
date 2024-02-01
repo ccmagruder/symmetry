320c1a6b63b4f72aef9c4b66db9d00b98eb2320d
Caleb Magruder <ccmagruder@gmail.com>
2024-01-31 21:17:24 -0600

    added : templated bComplexAddition

--- build.sh
+++ build.sh
@@ -1,10 +1,42 @@
 #!/bin/zsh
 
-# earthly --sat earthly-noble-arm64 -i +all
+# earthly sat ls
+#
+# if $?
+# then
+#   earthly account login --token $(cat /run/secrets/user_earthly_token)
+#   earthly org select ccmagruder
+#   earthly sat select earthly-noble-arm64
+# fi
+#
+# earthly +test
+
 if [ -d build ]; then
   rm -rf build
 fi
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -B build -S symmetry \
-  && cmake --build ./build \
-  && ctest --test-dir build
+export PATH=/opt/homebrew/bin:/usr/local/bin/:$PATH
+
+cmake -B build -S symmetry -DCMAKE_BUILD_TYPE=Debug
+
+if [ ! $? -eq 0 ]; then
+  echo "<<<<<<<< CMake Configure Failed >>>>>>>>"
+  exit 
+fi
+
+cmake --build build -j10
+
+if [ ! $? -eq 0 ]; then
+  echo "<<<<<<<< CMake Build Failed >>>>>>>>"
+  exit 1
+fi
+
+ctest --rerun-failed --output-on-failure --test-dir build
+
+if [ ! $? -eq 0 ]; then
+  echo "<<<<<<<< CTest Failed >>>>>>>>"
+  exit 1
+else
+  echo "<<<<<<<< Build and Test Passed >>>>>>>>"
+fi
+
