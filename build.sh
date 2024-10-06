d25f2298bdeed01523fbaa59b5d3394b77e662ca
Caleb Magruder <ccmagruder@gmail.com>
2024-10-06 18:25:30 -0500

    added : ccls local build

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 
-earthly --sat earthly-noble-arm64 -i +all
+# earthly --sat earthly-noble-arm64 -i +all
+if [ -d build ]; then
+  rm -rf build
+fi
 
-# earthly -a +build/compile_commands.json
-
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -B build -S symmetry \
+  && cmake --build ./build \
+  && ctest --test-dir build
