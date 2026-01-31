7cff5e7d3eb3a23a76f3f7edf81f559d75a0c9c8
ccmagruder <ccmagruder@gmail.com>
2026-01-31 16:58:20 -0600

    added : flake config w/o CUDA

---
+++ symmetry/default.nix
@@ -0,0 +1,53 @@
+# default.nix
+{ lib, stdenv, cmake, cudaPackages, gtest, json, gbenchmark}:
+
+stdenv.mkDerivation {
+  pname = "symmetry";
+  version = "0.0.0";
+
+  src = ./.;
+
+  requiredSystemFeatures = [ "cuda" ];
+  __noChroot = true;
+
+  nativeBuildInputs = [
+    cmake
+    cudaPackages.cuda_nvcc
+  ];
+
+  buildInputs = [
+    cudaPackages.cuda_cudart
+    gtest
+    json
+    gbenchmark
+  ];
+
+  doCheck = true;
+
+  checkPhase = ''
+    runHook preCheck
+    export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver/lib64:$LD_LIBRARY_PATH"
+
+    ls -l /run/opengl-driver/lib/libcuda.so.1 || echo "❌ Still missing libcuda in sandbox!"
+
+    if [ ! -L /run/opengl-driver ]; then
+      echo "❌ ERROR: /run/opengl-driver is missing in sandbox. Check nix.conf extra-sandbox-paths."
+    fi
+
+    ctest --test-dir . --output-on-failure
+    runHook postCheck
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    cmake --install . --prefix $out
+    runHook postInstall
+  '';
+
+
+  meta = with lib; {
+    description = "A CUDA Symmetry In Chaos Image Generator";
+    license = licenses.unfree; # CUDA requires this
+    platforms = platforms.linux;
+  };
+}
